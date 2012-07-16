8cd7bc9d43800e2a7710074f581b9389c777b527
Antonio Pardo <apardo@alabs.es>
2012-07-16 05:59:05 +0200

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subhackers::Application.config.secret_token = '37268a09bcffc50e927f2afd2dd2e44c236bcfd280237df3e215d05bcf8226eab44e5b44f20050a1614ea9fa0209b4e317c9df6efca1e7a124dc57ea3a727c8f'
